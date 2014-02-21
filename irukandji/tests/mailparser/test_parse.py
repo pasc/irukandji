77634900ce3952cb4d41f5440403f098a47e9b76
Pascal Hakim <pasc@redellipse.net>
2014-02-21 23:02:39 +1100

    Added parsing test with blank line

--- irukandji/tests/mailparser/test_parse.py
+++ irukandji/tests/mailparser/test_parse.py
@@ -8,6 +8,7 @@ test_data = {
         'l0_header': 'On 1/1/2013, John Smith wrote:\n',
         'l1': '> Hey,\n',
         'l1_header': '> On 2/1/2013, Mark Doe said:\n',
+        'l1_b': '>\n',
         'l2': '>> Text Text\n',
         }
 
@@ -31,6 +32,17 @@ class TestParse(unittest.TestCase):
         self.assertTrue(isinstance(ret, Message))
 
         self.assertEqual([0, 1, 0], map(lambda x: x.level, ret.content))
+
+    def test_simple_reply_with_blank(self):
+        message = ['l0', 'l0_header', 'l1_header', 'l1', 'l1_b', 'l1', 'l0']
+        message = ''.join(map(lambda x: test_data[x], message))
+
+        ret = generate_message(message, '', '')
+        self.assertTrue(isinstance(ret, Message))
+
+        print ret.content
+        self.assertEqual([0, 1, 1, 0], map(lambda x: x.level, ret.content))
+
 
     def test_simple_long(self):
         message = ['l0', 'l0_header', 'l1_header', 'l1', 'l1', 'l2', 'l1', 'l2', 'l0', 'l1', 'l0']
